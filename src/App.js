944634e0bd98f912ed35e30cf1a764f43204bff0
amogh007 <30520870+amogh007@users.noreply.github.com>
2022-12-16 03:02:29 +0530

    removed smooth scroll

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Body from './componenets/Body';
 import Dody from './componenets/Dody';
-import { SmoothProvider } from 'react-smooth-scrolling'
+
 import Navbar from './componenets/Navbar';
 import Preface from './componenets/Preface';
 
@@ -9,12 +9,12 @@ import Preface from './componenets/Preface';
 function App() {
   return (
     <div className="App bg-gradient-to-r from-[#d4d4d4] to-[#3d3d3d]">
-     <SmoothProvider skew={true}>
+     
       <Navbar/>
       <Body/>
       <Preface/>
       <Dody/>
-      </SmoothProvider>
+      
      
     </div>
   );
